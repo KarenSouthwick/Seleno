5280fc9dc4ed1c785a316b0878c7262a2bb4182a
KarenSouthwick <Karen.Southwick@Authenticateis.com>
2018-10-16 12:15:44 +0100

    Add project files.

---
+++ Seleno/BrowserHost.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestStack.Seleno.Configuration;
+using TestStack.Seleno.Configuration.WebServers;
+using System.Threading;
+
+namespace Seleno
+{
+    public static class BrowserHost
+    {
+        public static readonly SelenoHost Instance = new SelenoHost();
+        public static readonly string RootUrl;
+
+        static BrowserHost()
+        {
+            Instance.Run(configure => configure
+            .WithWebServer(new InternetWebServer("https://qa-platform.authenticateis.com/")));
+
+            RootUrl = Instance.Application.Browser.Url;
+                        
+        }
+    }
+}
---
+++ Seleno/Test1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using TestStack.Seleno.PageObjects.Locators;
+
+namespace Seleno
+{
+    [TestFixture]
+
+    public class Test1
+    {
+        [Test]
+
+        public void LogInTest()
+        {
+            BrowserHost.Instance.Application.Browser
+                .Navigate()
+                .GoToUrl(BrowserHost.RootUrl);
+
+            var firstNameBox = BrowserHost.Instance.Application.Browser.FindElement(By.Id("UserName"));
+            firstNameBox.SendKeys("karensouthwick");
+
+            var passwordBox = BrowserHost.Instance.Application.Browser.FindElement(By.Id("Password"));
+            passwordBox.SendKeys("Exchange!6");
+
+            var loginBox = BrowserHost.Instance.Application.Browser.FindElement(By.Id("do-submit"));
+            loginBox.Click();
+           
+        }
+    }
+}
