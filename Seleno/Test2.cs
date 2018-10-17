103cc88184b92cef69f4cc78b2e31200e3021f9e
KarenSouthwick <Karen.Southwick@Authenticateis.com>
2018-10-17 16:08:05 +0100

    test2 is a bit shorter than test1

---
+++ Seleno/Test2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using System.Threading;
+
+namespace Seleno
+{
+    class Test2
+    {
+        [Test]
+
+        public void LogIn2Test()
+        {
+            var logInPage = BrowserHost.Instance.NavigateToInitialPage<LogInPage>();
+
+            var signUpPage = logInPage.GoToSignUpPage();
+                    
+            Thread.Sleep(3000);
+
+        }
+    }
+}
